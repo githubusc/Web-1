924fa20fe7b4f7113923387dee381975021a5b8f
Qiang Wang <chianh7238@gmail.com>
2022-01-27 18:07:03 -0800

    Updating Homework

--- src/React/Homework/Essays/Essays.jsx
+++ src/React/Homework/Essays/Essays.jsx
@@ -6,13 +6,29 @@ const Essays = () => {
         <EssaysStyled>
             <h2>Essays</h2>
             <Essay q="S2: What is the difference between Git, Github and Heroku?" >
-                This is my answer.
+                <ul>
+                    <li>Git:Version control system, manage and edit code on their sysem; Free; Merge branches in once approved.</li>
+                    <li>Github: A place for our Git Repos -- remote server -- leveraging Git. Teamwork, code review, sharing.</li>
+                    <li>Heroku: Cloud Based Web (App) Server with the ability to deploy apps, without having a DevOps degree.</li>
+                </ul>
             </Essay>
             <Essay q="S3: Explain the difference between HTML, CSS and JS." >
-                Something else.
+                <ul>
+                    <li>HTML: Mark Up Language for tag based hierarchy and semantic definition.</li>>
+                    <li>CSS: The Skin (Appearance / Visual Asethetics), CSS Selectors -- to select html elements. </li>
+                    <li>JS: Programming language for Interactive Features of the site (Behavior) Slideshows, scrolling, </li>
+                </ul>
             </Essay>
             <Essay q="S4: What are Single Page Apps (SPAs)? How do they differ from traditional multi-page Web Sites?" >
-                Yet another.
+                <ul>
+                    <li>Single Page: Does not have to reload like multi-page (less repetition)</li>
+                    <li>Faster load time, more efficient.</li>
+                </ul>
+            </Essay>
+            <Essay q="S5: S5: What is the difference between Web Designer, Front End Developer and Back End develper?" >
+                <ul>
+                    <li>Answer</li>
+                </ul>
             </Essay>
 
         </EssaysStyled>
