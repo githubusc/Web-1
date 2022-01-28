47a1018c60c7af2ae94b3349c4a79d57ac969a43
Qiang Wang <chianh7238@gmail.com>
2022-01-27 20:14:58 -0800

    Styling Header

--- src/React/App.jsx
+++ src/React/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
-import Header from './Layout/Header';
+import Header from './Layout/Header/Header';
 import Nav from './Layout/Nav/Nav';
 import Main from './Layout/Main';
 import Footer from './Layout/Footer';
@@ -12,7 +12,6 @@ const App = () => {
     return (
         <BrowserRouter>
             <AppStyled className='App'>
-                App
                 <Header />
                 <Nav />
                 <Main />
@@ -25,13 +24,5 @@ const App = () => {
 export default App;
 
 const AppStyled = styled.div`
-    border: solid 10px purple;
-    padding: 20px;
-    max-width: 800px;
-    margin: auto;
 
-    div {
-        border: solid 5px teal;
-        padding: 20px;
-    }
 `;
