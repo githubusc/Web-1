0521decb39f25c5551494df96fdd967da00d37d3
Qiang Wang <chianh7238@gmail.com>
2022-01-31 18:44:01 -0800

    Styled Footer

--- src/React/App.jsx
+++ src/React/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './Layout/Header/Header';
 import Nav from './Layout/Nav/Nav';
 import Main from './Layout/Main';
-import Footer from './Layout/Footer';
+import Footer from './Layout/Footer/Footer';
 
 const App = () => {
 
--- src/React/Layout/Footer/Footer.jsx
+++ src/React/Layout/Footer/Footer.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
+import Logo from './Logo';
+import SiteTitle from './SiteTitle';
 
 const Footer = () => {
 
     return (
         <FooterStyled className='Footer'>
-            Footer 
+            <SiteTitle />
+            <Logo />
         </FooterStyled>
     );
 }
@@ -13,5 +16,7 @@ const Footer = () => {
 export default Footer;
 
 const FooterStyled = styled.footer`
-    
+    background-color: teal;
+    text-align: center;
+    padding: 20px 0px;
 `;
