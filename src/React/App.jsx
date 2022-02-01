45a5f93155b65e3732c7fc4c4f4209f876317051
Qiang Wang <chianh7238@gmail.com>
2022-01-31 20:42:49 -0800

    Adding media queries

--- src/React/App.jsx
+++ src/React/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 
+/* Scripts  ---------------------------*/
+import { MediaQueryProvider } from './common/useMediaQuery';
+
+/* Components ---------------------------*/
 import Header from './Layout/Header/Header';
 import Nav from './Layout/Nav/Nav';
 import Main from './Layout/Main';
@@ -10,14 +14,16 @@ import Footer from './Layout/Footer/Footer';
 const App = () => {
 
     return (
-        <BrowserRouter>
-            <AppStyled className='App'>
-                <Header />
-                <Nav />
-                <Main />
-                <Footer />
-            </AppStyled>
-        </BrowserRouter>
+        <MediaQueryProvider>
+            <BrowserRouter>
+                <AppStyled className='App'>
+                    <Header />
+                    <Nav />
+                    <Main />
+                    <Footer />
+                </AppStyled>
+            </BrowserRouter>
+        </MediaQueryProvider>
     );
 }
 
