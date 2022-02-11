fb142316f8b8bed19fc4f43bee25ef7ef6fcfefc
Qiang Wang <chianh7238@gmail.com>
2022-02-10 18:39:05 -0800

    Styling services

--- src/React/Services/ServiceGallery/ServiceGalleryItem.jsx
+++ src/React/Services/ServiceGallery/ServiceGalleryItem.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { servicesData } from '../servicesData';
 
+import { defaultMediaQueries } from 'React/common/useMediaQuery';
+
 const ServiceGalleryItem = ({service}) => {
 
     return (
@@ -15,5 +17,33 @@ const ServiceGalleryItem = ({service}) => {
 export default ServiceGalleryItem;
 
 const ServiceGalleryItemStyled = styled.div`
-    
+    width: 50%;
+    padding: 5px;
+    position: relative;
+
+    img {
+        width: 100%;
+    }
+
+    h3 {
+        background-color: rgba(0, 128, 128, 0.715);
+        margin: 0px;
+        padding: 10px;
+        font-size: 14px;
+        position: absolute;
+        left: 5px;
+        right: 5px;
+        bottom: 5px;
+    }
+
+    @media ${defaultMediaQueries.mdUp} {
+        width: 33%;
+        padding: 5px;
+        h3 {font-size: 16px;}
+    }
+    @media ${defaultMediaQueries.lgUp} {
+        width: 25%;
+        padding: 5px;
+        h3 {font-size: 18px;}
+    }
 `;
