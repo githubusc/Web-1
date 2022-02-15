0c9a929a5c41244015551678068724163c91c191
Qiang Wang <chianh7238@gmail.com>
2022-02-14 19:22:46 -0800

    Wiring up initial lightbox

--- src/React/Contact/Contact.jsx
+++ src/React/Contact/Contact.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/* Components ---------------------------*/
+import Lightbox from 'React/common/Lightbox/Lightbox';
+
 const Contact = () => {
 
+    const ActiveContent = () => {
+        return <img src="/assets/img/moon.png" />;
+    }
+
+    const ModalContent = () => {
+        return <img src="/assets/img/sun.png" />;
+    }
     return (
         <ContactStyled className='Contact'>
             <h1>Contact</h1>
+            <Lightbox
+                title="Sun Pop Up"
+                activeContent={<ActiveContent />}
+                modalContent={<ModalContent />}
+            />
         </ContactStyled>
     );
 }
