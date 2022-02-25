d341ee148625daaf3c6e4351dc4b800e0c8c6f99
Qiang Wang <chiang7238@gmail.com>
2022-02-24 18:50:14 -0800

    Uform Branch: Wrapping Ufrom in Context

--- src/React/common/UniversalForm/UniversalForm.jsx
+++ src/React/common/UniversalForm/UniversalForm.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import styled from 'styled-components';
 
+/* Context  ---------------------------*/
+import Context from './Context/index.js';
+import reducer from './Context/reducer.js';
 /* Components H3 ---------------------------*/
 import Form from './Form';
 import Feedback from './Feedback';
@@ -12,13 +15,21 @@ export { default as SubmitButton } from './Controls/SubmitButton.jsx'
 
 const UniversalForm = ({children}) => {
 
+    const defaultFormData = {
+        sample: 'My Sample Data',
+    };
+
+    const [state, dispatch] = useReducer(reducer, defaultFormData);
+
     return (
-        <UniversalFormStyled className='UniversalForm'>
-            <Feedback />
-            <Form>
-                {children}
-            </Form>
-        </UniversalFormStyled>
+        <Context.Provider value={ {state, dispatch} } displayName='Universal Form'>
+            <UniversalFormStyled className='UniversalForm'>
+                <Feedback />
+                <Form>
+                    {children}
+                </Form>
+            </UniversalFormStyled>
+        </Context.Provider>
     );
 }
 
