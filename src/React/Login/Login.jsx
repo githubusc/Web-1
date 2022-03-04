c249c9dd53801467b334806d4b01cfb35e430f14
Qiang Wang <chiang7238@gmail.com>
2022-03-03 18:27:52 -0800

    Adding Login

--- src/React/Login/Login.jsx
+++ src/React/Login/Login.jsx
@@ -1,13 +1,38 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+/* Components ---------------------------*/
+import Lightbox from 'React/common/Lightbox/Lightbox';
+import UniversalForm, { Input, SubmitButton } from 'React/common/UniversalForm/UniversalForm';
+
 const Login = () => {
 
-    
+    const onSubmitHandler = ({ fields, payload }) => {
+        console.log('onSubmit fields, payload', fields, payload);
+    }
+
     return (
         <LoginStyled className='Login'>
             <h1>Login</h1>
-
+            <UniversalForm
+                apiUrl='/user/login'
+                onSubmit={ onSubmitHandler }
+            >
+                <Input
+                    label='Username'
+                    id='username'
+                    type='text'
+                    placeholder='user@domain.com'
+                    rules={ ['required', 'email'] }
+                />
+                <Input
+                    label='Password'
+                    type='password'
+                    id='password'
+                    rules={ ['required'] }
+                />
+                <SubmitButton>Login</SubmitButton>
+            </UniversalForm>
         </LoginStyled>
     );
 }
